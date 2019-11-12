afd511e6965894c9d48d16e917ef687b808f9d80
Matthew Hwang <mh1160@ilab1.cs.rutgers.edu>
2019-11-11 20:19:40 -0500

    make a random list and scrambled it

--- multitest_thread.c
+++ multitest_thread.c
@@ -1,1 +1,6 @@
 //library with a multithread implementation of your search function
+
+struct thread{
+	pthread_t* th;
+	int retval;
+}
--- searchtest.c
+++ searchtest.c
@@ -1,1 +1,31 @@
 // implements both multiprocess and multithread searches based on how it is built
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(int argc, char** argv){
+	int size = 100; //supposed to test different ranges of sizes along with different step sizes
+	
+	int* arr = (int*)malloc(size*sizeof(int));
+	
+	
+	//generate list in sequence
+	for(int i = 0; i<size; i++){
+		arr[i] = i;
+	}
+	
+	int r; int temp;
+	//scramble list
+	for(int i = 0; i<size; i++){
+		r = rand() % size;
+		temp = arr[i];
+		arr[i] = arr[r];
+		arr[r] = temp;
+	}
+	
+	/*for(int i = 0; i<size; i++){
+		printf("%d\n", arr[i]);
+	}*/
+	
+	return 0;
+}
