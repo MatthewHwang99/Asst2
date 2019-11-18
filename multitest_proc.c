a8e43cf4f90863f41c16be16921c84ed2c2baf35
Noah M. Lipton <nml109@ice.cs.rutgers.edu>
2019-11-17 20:59:35 -0500

    did some stuff to my proc.c file

--- multitest_proc.c
+++ multitest_proc.c
@@ -6,13 +6,32 @@
 #include <unistd.h>
 #include <sys/types.h>
 
-int search(int *arr, int size, int numofProc, int index){
+int linearSearch(int* arr, int size, int target){
+  int j;
+  for(j = 0; j < size; j++){
+    if(target == arr[j]){
+      return j;
+    }
+  }
+  return -1;
+}
+
+
+int search(int *arr, int size, int target){
   //size refers to the size of the broken down initial array
-  int i, result;
+  int i, result, numofProc;
+  if(size < 250){
+    numofProc = 1;
+  }else if((size % 250) == 0){
+    numofProc = size / 250;
+  }else{
+    numofProc = size/250;
+    numofProc++;
+  }
   for(i = 0; i < numofProc; i++){
     if(fork() == 0){
       //child process; search a portion of the array
-      exit(search());//actual search function, returns the index; -1 if not found
+      exit(linearSearch(&arr[(size/numofProc) * i], (size/numofProc) , target));//actual search function, returns the index; -1 if not found
       //It will never return a value past 255 because of the 250 element array cap
     }else{
       wait(&result);
