654fe96a09c92b10e442a878e13c7784869d2e99
Noah M. Lipton <nml109@ice.cs.rutgers.edu>
2019-11-17 21:17:55 -0500

    troubleshooting changes?

--- searchtest.c
+++ searchtest.c
@@ -6,45 +6,44 @@
 void rescramble(int, int*, int);
 
 void rescramble(int prevIndex, int* arr, int size){
-	int r = rand() % size;
-	
-	int temp = arr[r];
-	arr[r] = arr[prevIndex];
-	arr[prevIndex] = temp;
-	
-	return;
+  int r = rand() % size;
+  
+  int temp = arr[r];
+  arr[r] = arr[prevIndex];
+  arr[prevIndex] = temp;
+  
+  return;
 }
 
 int main(int argc, char** argv){
-	int size = 20000; //supposed to test different ranges of sizes along with different step sizes
-	int* arr = (int*)malloc(size*sizeof(int));
-	int target = 14242;
-	
-	//generate list in sequence
+  int size = 20000; //supposed to test different ranges of sizes along with different step sizes
+  int* arr = (int*)malloc(size*sizeof(int));
+  int target = 2;
+  
+  //generate list in sequence
+  for(int i = 0; i<size; i++){
+    arr[i] = i;
+  }
+  
+  int r; int temp;
+  //scramble list
+  for(int i = 0; i<size; i++){
+    r = rand() % size;
+    temp = arr[i];
+    arr[i] = arr[r];
+    arr[r] = temp;
+  }
+  /*	
 	for(int i = 0; i<size; i++){
-		arr[i] = i;
+	printf("%d, ", arr[i]);
 	}
-	
-	int r; int temp;
-	//scramble list
-	for(int i = 0; i<size; i++){
-		r = rand() % size;
-		temp = arr[i];
-		arr[i] = arr[r];
-		arr[r] = temp;
-	}
-	/*	
-	for(int i = 0; i<size; i++){
-		printf("%d, ", arr[i]);
-	}
-	*/
-	//running the test 3 times to see if rescramble works
-	for(int i = 0; i<3; i++){
-	  int targetFound = search(arr, size, target);
-	  printf("%d\n", targetFound);
-	  rescramble(targetFound, arr, size);
-	  printf("i\n");
-	}
-	
-	return 0;
+  */
+  //running the test 3 times to see if rescramble works
+  for(int i = 0; i<3; i++){
+    int targetFound = search(arr, size, target);
+    //printf("%d\n", targetFound);
+    rescramble(targetFound, arr, size);
+  }
+  
+  return 0;
 }
