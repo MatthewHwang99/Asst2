9ce14c68364710c9aefba96fcd7d5ce95d35d3d1
Noah M. Lipton <nml109@ice.cs.rutgers.edu>
2019-11-19 17:46:10 -0500

    Correctly figured out how to divide of partitions

--- multitest_proc.c
+++ multitest_proc.c
@@ -19,25 +19,25 @@ int linearSearch(int* arr, int size, int target){
 
 int search(int *arr, int size, int target){
   //size refers to the size of the initial array
-  int i, result, numofProc;
-  int partitionSize = 5;
-  if(size < partitionSize){
-    numofProc = 1;
-  }else if((size % partitionSize) == 0){
-    numofProc = size / partitionSize;
-  }else{
-    numofProc = size/partitionSize;
+  int i, result, numofProc = 1, leftover;
+  while(size/numofProc > 250){
     numofProc++;
   }
+  leftover = size - (numofProc * (size/numofProc));
+
   for(i = 0; i < numofProc; i++){
     if(fork() == 0){
       //child process; search a portion of the array
-      exit(linearSearch(&arr[(size/numofProc) * i], (size/numofProc), target));//actual search function, returns the index; -1 if not found
+      if((numofProc - i) == 1 && leftover > 0){
+	exit(linearSearch(&arr[(size/numofProc) * i], leftover, target));
+      }
+      //actual search function, returns the index; -1 if not found
+      exit(linearSearch(&arr[(size/numofProc) * i], (size/numofProc), target));
       //It will never return a value past 255 because of the 250 element array cap
     }else{
       wait(&result);
     }
-
+    
     if(WEXITSTATUS(result) != 255){
       //Found the target
       //printf("Found the target %d at index %d of iteration %d\n", target, WEXITSTATUS(result), i);
@@ -47,3 +47,24 @@ int search(int *arr, int size, int target){
   //target not found
   return -1;
 }
+
+  /* 
+  int partitionSize = 0;
+  int maxparts = 250;
+  int tempSize = size;
+  while((tempSize/2) > 250){
+    partitionSize++;
+    tempSize/= 2;
+  }
+  if(tempSize = 1){
+    partitionSize++;
+  }
+  if(size < partitionSize){
+    numofProc = 1;
+  }else if((size % partitionSize) == 0){
+    numofProc = size / partitionSize;
+  }else{
+    numofProc = size/partitionSize;
+    numofProc++;
+  }
+*/
