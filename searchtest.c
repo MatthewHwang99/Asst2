620e2721d66e7634fc500185147aa8d9dba0654d
Noah M. Lipton <nml109@ice.cs.rutgers.edu>
2019-11-20 10:53:46 -0500

    cleaned up

--- searchtest.c
+++ searchtest.c
@@ -19,7 +19,7 @@ void rescramble(int prevIndex, int* arr, int size){
 }
 
 int main(int argc, char** argv){
-  int size = 250; //supposed to test different ranges of sizes along with different step sizes
+  int size = 20000; //supposed to test different ranges of sizes along with different step sizes
   int* arr = (int*)malloc(size*sizeof(int));
   int target = 25;
   srand(time(0));
@@ -36,73 +36,27 @@ int main(int argc, char** argv){
     arr[i] = arr[r];
     arr[r] = temp;
   }
-
   
   struct timeval start, end;
   unsigned long time = 0;
   //unsigned long time2 = 0;
-<<<<<<< HEAD
   int numRuns = 100;
   FILE *fp = fopen("resulttest.txt", "w");
-=======
-  int numRuns = 10;
-  
-  printf("Searching an array of size %d and running the search %d times\n", size, numRuns);
 
->>>>>>> bf8d692626117b9258c476f3204e4d0e9cafff12
-  //running the test 3 times to see if rescramble works
   for(int i = 1; i<=numRuns; i++){
     gettimeofday(&start, 0);
-<<<<<<< HEAD
-    int targetFound = (int)search(arr, size*i, target);
-    printf("Target %d found at index %d\n", target, targetFound);
-=======
     int targetFound = (int)search(arr, size, target);
-    printf("Iteration(%d): Target %d found at index %d\n", i, target, targetFound);
->>>>>>> bf8d692626117b9258c476f3204e4d0e9cafff12
+    printf("Target %d found at index %d\n", target, targetFound);
     gettimeofday(&end, 0);
     time += (end.tv_sec - start.tv_sec)*1000000.0 + end.tv_usec - start.tv_usec;
     fprintf(fp,"Size of array: %d Time: %lu\n" , i*size, time);
     rescramble(targetFound, arr, size);
-<<<<<<< HEAD
   } 
   
   unsigned long avgTime = time/numRuns;	
   fprintf(fp, "Average Time %lu\n", avgTime);
-  fclose(fp);
-  //time = time/3;
-  
-  /*for(int i = 0; i<10; i++){
-    gettimeofday(&start, 0);
-    for(int i = 0; i<size; i++){
-    if(arr[i] == target){
-    break;
-    rescramble(i, arr, size);
-    }
-    }
-    gettimeofday(&end, 0);
-    time2 += (end.tv_sec - start.tv_sec)*1000000.0 + end.tv_usec - start.tv_usec;
-    }*/
-  
-=======
-	} 
-
-  unsigned long avgTime = time/numRuns;	  
->>>>>>> bf8d692626117b9258c476f3204e4d0e9cafff12
-  
+  fclose(fp); 
   printf("Total run time: %lu microseconds.\nAverage time per search: %lu microseconds.\n", time, avgTime);
-  
-  //printf("Total run time: %lu microseconds.\n", time2);
-  
+
   return 0;
-  /*
-  //running the test 3 times to see if rescramble works
-  for(int i = 0; i<5; i++){
-    int targetFound = search(arr, size, target);
-    printf("Found target %d at index %d\n", target, targetFound);
-    rescramble(targetFound, arr, size);
-  }
-  
-  return 0;
-  */
 }
